8d3732a11514544335a7b07958a0e3575067d6af
Rob Thijssen <rthijssen@gmail.com>
2019-12-12 14:00:28 +0200

    Updates

---
+++ precache-manifest.b753ad624100c9af3358c10de297d6ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9f02ed9fc8fa877d60b4f5d6406f88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddc1d5c8f6e36519b143",
+    "url": "/static/css/2.3d28a078.chunk.css"
+  },
+  {
+    "revision": "84fcf2561572cea9d25e",
+    "url": "/static/css/main.a9c9ed8e.chunk.css"
+  },
+  {
+    "revision": "ddc1d5c8f6e36519b143",
+    "url": "/static/js/2.6a110c59.chunk.js"
+  },
+  {
+    "revision": "f95e37d0f2faaddca6848bd295e1e0e8",
+    "url": "/static/js/2.6a110c59.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84fcf2561572cea9d25e",
+    "url": "/static/js/main.db46f10d.chunk.js"
+  },
+  {
+    "revision": "1d003f886eb2d0911cfb",
+    "url": "/static/js/runtime-main.e5152cc3.js"
+  }
+]);
